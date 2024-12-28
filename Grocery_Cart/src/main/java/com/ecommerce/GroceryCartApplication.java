84d2f7c46da98eecbb91774330bb7a029705278a
MASTER <MASTER@DESKTOP-TN4CPM9>
2024-12-28 11:05:56 +0530

    project set up

---
+++ Grocery_Cart/src/main/java/com/ecommerce/GroceryCartApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryCartApplication.class, args);
+	}
+
+}
